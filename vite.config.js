e6724eb0eb2c60ebc3b6e756de88f4145430498e
deeksha <deekshagp14@gmail.com>
2025-07-06 11:43:22 +0530

    Updated portfolio section

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'ShreeVision',  // 👈 repo name here
+  base: '/ShreeVision/',  // 👈 repo name here
   plugins: [react()],
 });
